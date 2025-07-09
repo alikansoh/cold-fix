bdd9a415b9b2eecac8f8a900dd28ca3715449ea6
alikansoh <alikanso.dev@gmail.com>
2025-07-09 16:05:13 +0100

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://coldfix.co.uk',
+    generateRobotsTxt: true,
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
