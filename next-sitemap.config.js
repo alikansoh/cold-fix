81dbb03873420c4e8b4570c6e9ad7efa137bd324
alikansoh <alikanso.dev@gmail.com>
2026-04-06 15:19:21 +0100

    change from coldfix to freezfix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://freezfix.co.uk',
-    generateRobotsTxt: true,
-    changefreq: 'weekly',
-    priority: 0.7,
-    sitemapSize: 5000,
-  };
-  
+  siteUrl: 'https://www.freezfix.co.uk', // ← match your canonical www domain
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  exclude: ['/404', '/500', '/api/*'],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+  },
+};
